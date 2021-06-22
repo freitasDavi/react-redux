2c5c192335bca7bca15b65127ee17ce37175a789
Davi <davi.freitas@useall.com.br>
2021-06-22 13:57:34 -0300

    add products reducer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,42 +1,29 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Wrapper, Container } from './App.styles'
+
 import LineChart from '../../shared/LineChart'
+
 import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader'
 import ShoppingList from '../ShoppingList'
-import { Wrapper, Container } from './App.styles'
-import productsMock from '../../mocks/products.json'
-import extractPercentage from '../../utils/extractPercentage'
 import Calculator from '../Calculator'
 
+import extractPercentage from '../../utils/extractPercentage'
+
+import { selectAllProducts, selectSelectedProducts, selectSelectedProductsTotalPrice } from '../../store/Products/Products.selectors'
+import { toggleProduct } from '../../store/Products/Products.actions'
+
 function App () {
-  const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+  const dispatch = useDispatch();
+  const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61'];
+  
+  const products = useSelector(selectAllProducts);
+  const selectedProducts = useSelector(selectSelectedProducts);
+  const totalPrice = useSelector(selectSelectedProductsTotalPrice);
 
-  const [products, setProducts] = useState(productsMock.products)
-  const [selectedProducts, setSelectedProducts] = useState([])
-  const [totalPrice, setTotalPrice] = useState(0)
-
-  useEffect(() => {
-    const newSelectedProducts = products
-      .filter(product => product.checked)
-    
-    setSelectedProducts(newSelectedProducts)
-  }, [products])
-
-  useEffect(() => {
-    const total = selectedProducts
-      .map(product => product.price)
-      .reduce((a, b) => a + b, 0)
-
-    setTotalPrice(total)
-  }, [selectedProducts])
-
-  function handleToggle (id, checked, name) {
-    const newProducts = products.map(product =>
-        product.id === id
-          ? { ...product, checked: !product.checked }
-          : product
-    )
-    setProducts(newProducts)
+  function handleToggle (id) {
+    dispatch(toggleProduct(id))
   }
 
   return <Wrapper>
